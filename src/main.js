41bc2324a30e79f5e59e597349e948c22a392ad1
Akshit Kumar <akshitcreates9@gmail.com>
2026-09-14 20:34:49 +0530

    Build auth SPA navigation

--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
-import './style.css'
-import * as THREE from 'three'
+import "./style.css";
+import * as THREE from "three";
+const home = document.getElementById("home");
+const auth = document.getElementById("auth");
+const signupBtn = document.querySelector(".signup");
+const loginBtn = document.querySelector(".login");
+
+function showSection(section) {
+  let sections = [home, auth];
+  sections.forEach((element) => {
+    element.classList.add("hidden");
+  });
+  const targetSection = document.getElementById(section);
+  targetSection.classList.remove("hidden");
+}
+showSection("home")
+
+signupBtn.addEventListener("click", () => {
+  showSection("auth");
+});
